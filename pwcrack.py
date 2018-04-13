353932caca3a2b286b8ee006b81e603ff1f52341
UN1X00 <un1x00@mail.com>
2018-04-13 18:41:14 +0100

    Update pwcrack.py

--- pwcrack.py
+++ pwcrack.py
@@ -1,25 +1,28 @@
 import crypt
 import sys
 import time
+import optparse
+from threading import Thread
+
 
 class bcolours:
     GREEN = '\033[92m'
     TURQ = '\033[96m'
     ENDC = '\033[0m'
 
-def testPass(user, cryptPass):
+def testPass(user, cryptPass, wname):
     salt = cryptPass[0:2]
     if not salt in ('HX','$6','$s'):
          print ('[*] Sorry, hash not recognised, moving onto next hash')
          return
     shasalt = cryptPass[3:11].strip()
     insalt = '$6$' + shasalt
-    dictFile = open(sys.argv[2],'r')
+    dictFile = open(wname,'r')
     dictLen = 0
     for line in dictFile.readlines():
         dictLen += 1
     fileLen = dictLen
-    dictFile = open(sys.argv[2],'r')
+    dictFile = open(wname,'r')
     currLine = 0
     for word in dictFile.readlines():
         currLine += 1
@@ -38,18 +41,26 @@ def testPass(user, cryptPass):
     return
 
 def main():
-    if len(sys.argv) != 3:
-        print ('usage: pwcrack.py <hashes file> <wordlist file>')
+    parser = optparse.OptionParser('usage: pwcrack.py ' + '-f <file with hash> -w <wordlist>')
+    parser.add_option('-f', dest='fname',type='string',help='specify a file with hashes inside')
+    parser.add_option('-w', dest='wname',type='string',help='specify a wordlist for dictionary attack')
+    (options,args) = parser.parse_args()
+    if (options.fname == None) | (options.wname == None):
+        print parser.usage
         exit(0)
+    else:
+        fname = options.fname
+        wname = options.wname
     count = 0
-    passFile = open(sys.argv[1],'r')
+    passFile = open(fname,'r')
     for line in passFile.readlines():
         if ":" in line:
             user = line.split(":")[0]
             cryptPass = line.split(":")[1].strip(' ')
             print ('\n' + '[*] Cracking Password for username: ' + user + '\n')
             time.sleep(2)
-            testPass(user, cryptPass)
+            t = Thread(target=testPass, args=(user, cryptPass, wname))
+            t.start()
 
 if __name__ == "__main__":
     main()
